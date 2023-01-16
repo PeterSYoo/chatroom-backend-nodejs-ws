cd782b7fd9431427e0660b640dcda86f4ba59b9a
Peter Yoo <trustintofu@gmail.com>
2023-01-16 00:39:04 -0800

    feat: Sets up socket io connection.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.APP,
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User Disconnected', socket.id);
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server started on port 3000');
+server.listen(process.env.PORT || 3001, () => {
+  console.log('Server started');
 });
