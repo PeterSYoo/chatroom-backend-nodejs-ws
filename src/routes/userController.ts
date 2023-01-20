f62f5d610ae35736564ed123f20d7ea492a406d2
Peter Yoo <trustintofu@gmail.com>
2023-01-20 02:18:29 -0800

    feat: Creates user endpoint controller file.

--- src/routes/userController.ts
+++ src/routes/userController.ts
@@ -1,8 +1,22 @@
+import Users from '../models/Users';
+
 const express = require('express');
 const user = express();
 
-user.get('/users', (req, res) => {
-  res.send('Users Endpoint');
+user.get('/user/get-users', async (req, res) => {
+  try {
+    const users = await Users.find({});
+    if (!users) return res.status(404).json({ error: 'Users not Found' });
+    if (users) return res.status(200).json(users);
+  } catch (error) {
+    res.status(404).json({ error: 'Error While Fetching Users' });
+  }
+});
+
+// POST - Save user document after google sign in through firebase
+user.post('/user/create-users', async (req, res) => {
+  try {
+  } catch (error) {}
 });
 
 module.exports = user;
